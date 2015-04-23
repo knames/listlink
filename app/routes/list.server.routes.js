7f296c9386b18ad92cf8703543ec8a9fa9a3c251
Assisting <erik@applied-logistics.org>
2015-04-23 16:15:22 -0600

    fixing semi-colons

--- app/routes/list.server.routes.js
+++ app/routes/list.server.routes.js
@@ -10,12 +10,12 @@ module.exports = function(app) {
 	// Article Routes
 	app.route('/list/:username')
 		.get(users.requiresLogin, list.hasAuthorization, list.getLists)
-		.post(users.requiresLogin, list.hasAuthorization, list.create);
+		.post(users.requiresLogin, list.hasAuthorization, list.create)
     .put(users.requiresLogin, list.hasAuthorization, list.update);
 
   app.route('/list/:editList')
     .put(users.requiresLogin, list.hasAuthorization, list.update)
-    .delete(users.requiresLogin, list.hasAuthorization, list.remove)
+    .delete(users.requiresLogin, list.hasAuthorization, list.remove);
 
 	// Finish by binding the article middleware
 	app.param('username', list.getLists);
