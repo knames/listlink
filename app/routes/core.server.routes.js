dcac914bd64547ba3b491fda97cfc1124e678796
Assisting <erik@applied-logistics.org>
2015-04-24 16:18:35 -0600

    Whoops

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -4,8 +4,4 @@ module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core.server.controller');
 	app.route('/').get(core.index);
-
-	if(window.history && window.history.pushState){
-    $locationProvider.html5Mode(true);
-  }
 };
