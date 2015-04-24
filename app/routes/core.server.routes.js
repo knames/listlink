b78b3db74dd69e93f2628fa2c3e3801d0a616c03
Assisting <erik@applied-logistics.org>
2015-04-24 16:16:51 -0600

    Changing Angular setup

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -4,5 +4,8 @@ module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core.server.controller');
 	app.route('/').get(core.index);
-	app.route('/#').get(core.index);
+
+	if(window.history && window.history.pushState){
+    $locationProvider.html5Mode(true);
+  }
 };
